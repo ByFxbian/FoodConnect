db0a5733b4698f8a5bfb7d598e81aadb94dd0dba
fabian.sopa <fabian.sopa@gmx.at>
2025-03-08 18:13:32 +0000

    Animation

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,6 @@
 
-import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:google_fonts/google_fonts.dart'; // Import Google Fonts package
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -10,32 +9,53 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen>
     with SingleTickerProviderStateMixin {
-  late AnimationController _animationController;
+  late AnimationController _logoAnimationController;
+  late AnimationController _dotsAnimationController;
   late Animation<double> _scaleAnimation;
   late Animation<double> _opacityAnimation;
+  late Animation<int> _dotsAnimation;
 
   @override
   void initState() {
     super.initState();
-    _animationController = AnimationController(
+
+    // Logo Animation Controller
+    _logoAnimationController = AnimationController(
       vsync: this,
       duration: Duration(milliseconds: 1500),
     )..repeat(reverse: true);
 
+    // Scale Animation for Logo
     _scaleAnimation = Tween<double>(begin: 0.95, end: 1.05).animate(
-      CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
+      CurvedAnimation(
+          parent: _logoAnimationController, curve: Curves.easeInOut),
     );
 
+    // Opacity Animation for Text
     _opacityAnimation = Tween<double>(begin: 0.7, end: 1.0).animate(
-      CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
+      CurvedAnimation(
+          parent: _logoAnimationController, curve: Curves.easeInOut),
+    );
+
+    // Dots Animation Controller
+    _dotsAnimationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 1000),
+    )..repeat();
+
+    // Dots Animation
+    _dotsAnimation = IntTween(begin: 0, end: 3).animate(
+      CurvedAnimation(parent: _dotsAnimationController, curve: Curves.easeInOut),
     );
   }
 
   @override
   void dispose() {
-    _animationController.dispose();
+    _logoAnimationController.dispose();
+    _dotsAnimationController.dispose();
     super.dispose();
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -48,15 +68,20 @@ class _LoadingScreenState extends State<LoadingScreen>
   }
 
   Widget _buildLoadingContent() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        _buildAnimatedLogo(),
-        SizedBox(height: 30),
-        _buildLoadingText(),
-        SizedBox(height: 30),
-        _buildLoadingIndicator(),
-      ],
+    return AnimatedBuilder(
+      animation: _dotsAnimationController,
+      builder: (context, child) {
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _buildAnimatedLogo(),
+            SizedBox(height: 30),
+            _buildLoadingText(),
+            SizedBox(height: 30),
+            _buildLoadingIndicator(),
+          ],
+        );
+      },
     );
   }
 
@@ -73,22 +98,25 @@ class _LoadingScreenState extends State<LoadingScreen>
     );
   }
 
-  Widget _buildLoadingText() {
-    return AnimatedOpacity(
-      opacity: _opacityAnimation.value,
-      duration: Duration(milliseconds: 500),
-      child: Text(
-      "Einen Moment bitte...",
-      style: GoogleFonts.poppins(
-        fontSize: 18,
-        fontWeight: FontWeight.bold,
-        color: Theme.of(context).colorScheme.onSurface,
-      ),
-    ),
-      );
+ Widget _buildLoadingText() {
+  String dots = "";
+  for (int i = 0; i < _dotsAnimation.value; i++) {
+    dots += ".";
   }
+  return AnimatedOpacity(
+    opacity: _opacityAnimation.value,
+    duration: Duration(milliseconds: 500),
+    child: Text(
+      "Einen Moment bitte$dots",
+      style: GoogleFonts.poppins(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+          color: Theme.of(context).colorScheme.onSurface),
+    ),
+  );
+}
   Widget _buildAnimatedLogo() {
-    return ScaleTransition(
+        return ScaleTransition(
         scale: _scaleAnimation,
         child: Image.asset(
           'assets/app_icon.png',
@@ -96,6 +124,6 @@ class _LoadingScreenState extends State<LoadingScreen>
           height: 150,
           fit: BoxFit.contain,
         ),
-    );
+      );
   }
 }
